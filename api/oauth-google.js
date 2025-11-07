0d6b3370a499e14b75932c92254a33f0f5c203c6
Simon Lusnak <simon@beehive.local>
2025-11-07 11:58:12 +0000

    Remove method checks from OAuth redirects

--- api/oauth-google.js
+++ api/oauth-google.js
@@ -1,8 +1,4 @@
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
   const redirectUri = `${process.env.NEXTAUTH_URL || 'https://ebeehive.vercel.app'}/api/oauth-callback`;
   
   const params = new URLSearchParams({
