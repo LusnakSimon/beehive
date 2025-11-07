1c917c14db91722135bdb68183b3390f1e936ef5
Simon Lusnak <simon@beehive.local>
2025-11-07 11:49:12 +0000

    Fix: Wrap NextAuth in async handler function

--- api/auth/[...nextauth].js
+++ api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
-export default NextAuth({
+const options = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -50,5 +50,8 @@ export default NextAuth({
   },
   
   secret: process.env.NEXTAUTH_SECRET,
-  debug: true,
-});
+};
+
+export default async function auth(req, res) {
+  return await NextAuth(req, res, options);
+}
